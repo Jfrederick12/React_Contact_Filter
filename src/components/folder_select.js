0358528238c25a6c1329566e8a36fa902d89020f
Justin Frederick <jfrederickdesign@gmail.com>
2017-05-01 17:02:13 -0500

    add color changes and transition effect to select options

--- src/components/folder_select.js
+++ src/components/folder_select.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../styles/folder_select.css';
 import '../styles/results_list.css';
 
 class FolderSelect extends Component {
@@ -7,6 +8,7 @@ class FolderSelect extends Component {
 			event.target.value,
 			this.props.contact
 		)
+		this.setState({ style: 'active'})
 	}
 
 	render() {
@@ -31,16 +33,20 @@ class FolderSelect extends Component {
 			<div className="col">
 				{
 					this.props.contact.organize ? 
-						<select disabled className="folder-option">
-							<option value={contactFolder}>{contactFolder}</option>
-						</select>
+						<div className={`folder-option ${contactFolder}`} >
+							<select disabled>
+								<option value={contactFolder}>{contactFolder}</option>
+							</select>
+						</div>
 					:
-						<select className="folder-option" onChange={ this.handleFolderChange.bind(this)}>
-							<option value={contactFolder}>{contactFolder}</option>
-							{filteredFolders.map((folder) => {
-								return <option key={folder} value={folder}>{folder}</option>
-							})}
-						</select>						
+						<div className={`folder-option ${contactFolder}`}>
+							<select onChange={ this.handleFolderChange.bind(this)}>
+								<option value={contactFolder}>{contactFolder}</option>
+								{filteredFolders.map((folder) => {
+									return <option key={folder} value={folder}>{folder}</option>
+								})}
+							</select>
+						</div>						
 				}
 			</div>
 		)
