f5836d2f658699f82d3e13a779d6c2061a5d488a
Justin Frederick <jfrederickdesign@gmail.com>
2017-05-02 15:00:22 -0500

    add social links

--- src/components/header.js
+++ src/components/header.js
@@ -5,6 +5,12 @@ class Header extends Component {
 	render() {
 		return(
 			<div>
+				<div className="social-icon">
+					<nav>
+						<a href='https://github.com/Jfrederick12/React_Contact_Filter' className="social-icon"><i className="fa fa-github-alt fa-2x"></i></a>
+						<a href='http://jfrederickportfolio.herokuapp.com/'>www.justin-frederick.com</a>
+					</nav>
+				</div>
 				<h1>Contact-o-Matic</h1>
 			</div>
 		)
